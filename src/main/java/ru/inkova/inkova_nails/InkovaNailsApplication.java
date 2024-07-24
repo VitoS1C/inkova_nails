75ca190a4d0d64baa390d7fd9e4af1e7458e0b2f
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-07-24 14:51:58 +0500

    Init project

---
+++ src/main/java/ru/inkova/inkova_nails/InkovaNailsApplication.java
@@ -0,0 +1,13 @@
+package ru.inkova.inkova_nails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkovaNailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkovaNailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/inkova/inkova_nails/InkovaNailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inkova.inkova_nails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkovaNailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
